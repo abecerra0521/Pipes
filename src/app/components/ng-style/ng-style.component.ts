15524d397749ae7d7a317c7edac5d201f2468cb6
Andrés Becerra Gómez <abecerra.aabg@gmail.com>
2018-06-28 11:52:08 -0500

    first commit

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+    <p [style.fontSize.px]="size">
+      Hola mundo
+    </p>
+
+    <button class="btn btn-primary" (click)="size = size + 2">
+      <i class="fa fa-plus"></i>
+    </button>
+    <button class="btn btn-primary" (click)="size = size - 2">
+      <i class="fa fa-minus"></i>
+    </button>
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  size: number = 10;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
